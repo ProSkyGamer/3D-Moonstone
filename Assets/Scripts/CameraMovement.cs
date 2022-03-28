980ebd80ecf68fe824fe573a526b02360b8825b5
ProSkyGamer <kozhukhar_2003@mail.ru>
2022-03-28 21:23:26 +0300

    0.0000

---
+++ Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    public float speed;
+    private Vector3 coord;
+    private Vector3 dir;
+
+    void Start()
+    {
+        SwipeDetection.SwipeEvent += OnSwipe;
+    }
+
+    void Update()
+    {
+        if (coord != transform.position && coord != Vector3.zero)
+        {
+            transform.position = Vector3.Lerp(transform.position, coord, Time.deltaTime);
+        }
+        if (transform.position.x >= 800)
+        {
+            coord.x = 800;
+            transform.position = Vector3.Lerp(transform.position, coord, Time.deltaTime);
+        }
+        else if (transform.position.x <= -10)
+        {
+            coord.x = 0;
+            transform.position = Vector3.Lerp(transform.position, coord, Time.deltaTime);
+        }
+        else if (transform.position.z >= 800)
+        {
+            coord.z = 800;
+            transform.position = Vector3.Lerp(transform.position, coord, Time.deltaTime);
+        }
+        else if (transform.position.z <= -10)
+        {
+            coord.z = 0;
+            transform.position = Vector3.Lerp(transform.position, coord, Time.deltaTime);
+        }
+    }
+
+    private void OnSwipe(Vector3 direction)
+    {
+        direction.z = direction.y;
+        direction.y = transform.position.y;
+        if (transform.position.y >= 10 && transform.position.y <= 30)
+            dir = direction * 10;
+        else
+            dir = direction * 20;
+        Move(dir);
+    }
+
+    private void Move(Vector3 dir)
+    {
+        coord = new Vector3(Mathf.Lerp(transform.position.x, transform.position.x + dir.x, speed * Time.deltaTime), transform.position.y, Mathf.Lerp(transform.position.z, transform.position.z + dir.z, speed * Time.deltaTime));
+    }
+
+}
